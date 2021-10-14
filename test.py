2991946c57c80ea737bcf977d530a0909067bac0
taskma <taskma@gmail.com>
2021-10-14 17:38:13 +0300

    develop

--- test.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017/")
-
-# db = client.test
-db = client["crypto_db"]
-collection = db["crypto_series"]
-datas = {"test1": "testyigit"}
-rec_id1 = collection.insert_one(datas)
-print("Data inserted with record ids", rec_id1)
